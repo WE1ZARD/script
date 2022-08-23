010d751808197114755c259b4624602ec4a65def
WEIZARD <111175406+WE1ZARD@users.noreply.github.com>
2022-08-23 14:04:02 +0800

    Add files via upload
    smart bandage

---
+++ bandage.lua
@@ -0,0 +1,41 @@
+--[[
+**********************************************
+
+    Smart Bandage
+    1. bandage mouseover Ally without change target
+    2. bandage Ally
+    3. bandage self
+
+**********************************************
+]]
+
+local L={}
+do
+    if (GetLocale()=="zhCN") then
+        L["Bandage"]="绷带"
+    else
+        L["Bandage"]="Bandage"
+    end
+end
+
+Bandage = function()
+    for b=0,4 do 
+        for s=1,GetContainerNumSlots(b,s)do 
+            local n=GetContainerItemLink(b,s)
+            if n and (strfind(n,L["Bandage"]) or strfind(n,"The Rag"))then 
+                if UnitExists("mouseover") and not UnitCanAttack("player","mouseover")then
+                    TargetUnit("mouseover")
+                    UseContainerItem(b,s)
+                    SpellTargetUnit("mouseover")
+                    TargetLastTarget()
+                elseif UnitExists("target") and not UnitCanAttack("player","target")then
+                    UseContainerItem(b,s)
+                    SpellTargetUnit("target")
+                else  
+                    UseContainerItem(b,s)
+                    SpellTargetUnit("player")
+                end                    
+            end 
+        end 
+    end
+end
