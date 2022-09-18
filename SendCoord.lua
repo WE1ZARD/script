39dd410b2fb7962233bc9ce49b3d97288e24298d
WEIZARD <111175406+WE1ZARD@users.noreply.github.com>
2022-09-18 22:05:13 +0800

    Create SendCoord.lua

---
+++ SendCoord.lua
@@ -0,0 +1,19 @@
+--[[ 
+====================================== 
+Send Coord
+  - Send Coord click MinimapZoneText (solo /in patry /in raid)
+====================================== 
+]]
+local function SendCoord()
+    local _,class = UnitClass('player')
+    local pX, pY = GetPlayerMapPosition("player")
+    if GetNumPartyMembers() > 5 then
+        SendChatMessage( format(GetZoneText()..' - (%.0f,%.0f)', pX*100, pY*100), "RAID" )
+    elseif GetNumPartyMembers() > 0 then 
+        SendChatMessage( format(GetZoneText()..' - (%.0f,%.0f)', pX*100, pY*100), "PARTY" )    
+    else
+        print( format('|c'..RAID_CLASS_COLORS[class].colorStr..GetZoneText()..' - |r(%.0f,%.0f)', pX*100, pY*100) )
+    end
+end
+
+MinimapZoneTextButton:SetScript("OnClick",SendCoord)
