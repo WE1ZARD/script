5393f18119a1d9d92f2098829a77fb4a862a4609
WEIZARD <111175406+WE1ZARD@users.noreply.github.com>
2022-09-18 23:47:10 +0800

    support mouseover on unitframe

--- snowfall.lua
+++ snowfall.lua
@@ -1,10 +1,68 @@
 --[[
-=====================================================
-Snowfall
-1. cast on key down
-2. enable spam attack/shot/wand
-=====================================================
+    ********************************************
+        Snowfall
+        1. cast on key down
+        2. spam casting shot/attack
+        mouseover
+        1. allow to mouseover on unitframe
+    ********************************************
 ]]
+
+local mouseover
+
+do
+    local orig = GameTooltip.SetUnit
+    function GameTooltip:SetUnit(unit)
+        mouseover = unit
+        return orig(self, unit)
+    end
+end
+
+do
+    local orig = GameTooltip.FadeOut
+    function GameTooltip:FadeOut()
+        mouseover = nil
+        return orig(self)
+    end
+end
+
+do
+    local orig = GameTooltip.Hide
+    function GameTooltip:Hide()
+        mouseover = nil
+        return orig(self)
+    end
+end
+
+do
+    local pass = function() end
+    local orig = UseAction
+    function UseAction(unit, clicked, onself)
+        local mouseover = mouseover
+        if mouseover and mouseover ~= 'target' then
+            local _PlaySound = PlaySound
+            local target = UnitName'target'
+            PlaySound = pass
+            ClearTarget()
+            PlaySound = _PlaySound
+            do
+                local autoSelfCast = GetCVar'autoSelfCast'
+                SetCVar('autoSelfCast', nil)
+                orig(unit, clicked, onself)
+                SetCVar('autoSelfCast', autoSelfCast)
+            end
+            SpellTargetUnit(mouseover)
+            if target then
+                PlaySound = pass
+                TargetLastTarget()
+                PlaySound = _PlaySound
+            end
+        else
+            orig(unit, clicked, onself)
+        end
+    end
+end
+
 function ActionButtonDown(id)
     if ( BonusActionBarFrame:IsShown() ) then
         local button = getglobal("BonusActionButton"..id);
